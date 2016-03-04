81abf732db5895f5574b741a0669bd379e17aa33
squidHandz <larrydigscoupons@gmail.com>
2016-03-03 17:55:46 -0700

    honey.nut.cheerios

--- daydream.js
+++ daydream.js
@@ -24,7 +24,7 @@ var request = require('request');
 
 var positions = ['qb', 'rb', 'wr', 'te'];
 var root_url = "http://www.fantasypros.com/nfl/rankings/";
-var positions;
+var position;
 for (pos in positions) {
   position = positions[pos];
   var url = root_url + position + '.php';
@@ -33,9 +33,8 @@ for (pos in positions) {
       throw err;
     } else {
       $ = cheerio.load(body);
-      // debugger
+      
       $('.table').find('tbody').last().find('tr').each(function(index){
-        //console.log($(this).find('td:nth-child(2)').text());
 
         var text = $(this).find('td:nth-child(2)').text().split(' ');
         var name = text[0] + ' ' + text[1];
